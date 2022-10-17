2b9f7abe3cfb54102d17a949b5838b510c136f2f
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-10-17 15:00:57 +0200

    Committed automatically.

---
+++ src/playground.js
@@ -0,0 +1,22 @@
+import '../public/globals.css'
+
+export var Playground = () => {
+  return (
+    <div>
+      <div
+        style={{
+          height: 150,
+          position: 'absolute',
+          left: 154,
+          top: 134,
+        }}
+      >
+        <img
+          src='https://github.com/concrete-utopia/utopia/blob/master/editor/resources/editor/pyramid_fullsize@2x.jpg?raw=true'
+          alt='Utopia logo'
+          style={{ height: '100%' }}
+        />
+      </div>
+    </div>
+  )
+}
