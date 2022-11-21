c7ad7c48d33180f7bc03604f3e4582efb4c89e9d
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-21 13:45:04 +0100

    pushpush

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -2,22 +2,10 @@ import * as React from 'react'
 import { Scene, Storyboard } from 'utopia-api'
 import { App } from '/src/app.js'
 import { Playground } from '/src/playground.js'
+import { View } from 'utopia-api'
 
 export var storyboard = (
   <Storyboard data-uid='0cd'>
-    <Scene
-      style={{
-        width: 700,
-        height: 759,
-        position: 'absolute',
-        left: 110,
-        top: 169,
-      }}
-      data-label='Playground'
-      data-uid='3fc'
-    >
-      <Playground style={{}} data-uid='d9d' />
-    </Scene>
     <Scene
       style={{
         width: 744,
@@ -31,5 +19,40 @@ export var storyboard = (
     >
       <App style={{}} data-uid='a28' />
     </Scene>
+    <View
+      style={{
+        backgroundColor: '#0091FFAA',
+        position: 'absolute',
+        left: 115,
+        top: 159,
+        width: 195,
+        height: 381,
+      }}
+      data-uid='efd'
+    >
+      <View
+        style={{
+          backgroundColor: '#0091FFAA',
+          position: 'absolute',
+          left: 30,
+          top: 67,
+          width: 99,
+          height: 429,
+        }}
+        data-uid='eac'
+      >
+        <View
+          style={{
+            backgroundColor: '#0091FFAA',
+            position: 'absolute',
+            left: 18,
+            top: 226,
+            width: 48,
+            height: 530,
+          }}
+          data-uid='551'
+        />
+      </View>
+    </View>
   </Storyboard>
 )
