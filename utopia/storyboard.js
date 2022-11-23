22efd80bead2f14a1be052066243b88c9de9fb17
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-23 14:29:25 +0100

    yeah

---
+++ utopia/storyboard.js
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+import { App } from '/src/app.js'
+import { Playground } from '/src/playground.js'
+import { View } from 'utopia-api'
+
+export var storyboard = (
+  <Storyboard data-uid='0cd'>
+    <View
+      style={{
+        backgroundColor: '#0091FFAA',
+        position: 'absolute',
+        left: 203,
+        top: 6,
+        width: 347,
+        height: 248,
+      }}
+      data-uid='b95'
+    >
+      <img
+        data-aspect-ratio-locked
+        src='./assets/Screenshot 2022-11-23 at 2.28.08 PM.png'
+        style={{
+          position: 'absolute',
+          width: 160,
+          height: 144,
+          top: 52,
+          left: 93.5,
+        }}
+        data-uid='602'
+      />
+    </View>
+  </Storyboard>
+)
