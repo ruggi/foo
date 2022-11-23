5d36f5040460ae157a456aed4b014111f97ec24d
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-23 14:55:26 +0100

    finder

---
+++ utopia/storyboard.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+import { App } from '/src/app.js'
+import { Playground } from '/src/playground.js'
+
+export var storyboard = (
+  <Storyboard data-uid='0cd'>
+    <img
+      alt=''
+      src='./assets/finder.png'
+      style={{
+        position: 'absolute',
+        left: -68,
+        top: 1,
+        width: 152,
+        height: 134,
+      }}
+      data-uid='68d'
+      data-aspect-ratio-locked
+    />
+  </Storyboard>
+)
