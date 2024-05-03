12f9543aca5d40b4d72ddbe9505587a51bebe2f3
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-05-03 11:30:35 +0200

    sss

--- app/routes/_index.js
+++ app/routes/_index.js
@@ -3,7 +3,7 @@ import { Link } from '@remix-run/react'
 export default function Index() {
   return (
     <div>
-      Hello index SLASHES
+      Hello index SLASHESss
       <Link to='/something'>Link me</Link>
     </div>
   )
