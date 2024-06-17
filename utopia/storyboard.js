8b09e164e5297f8b0e053e115cd5f40e0d57331d
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-06-17 13:13:26 +0200

    woah

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -18,5 +18,30 @@ export var storyboard = (
       data-uid='68d'
       data-aspect-ratio-locked
     />
+    <div
+      style={{
+        backgroundColor: '#aaaaaa33',
+        position: 'absolute',
+        left: 170,
+        top: 101,
+        width: 146,
+        height: 60,
+      }}
+      data-uid='1e6'
+    >
+      <span
+        style={{
+          position: 'absolute',
+          wordBreak: 'break-word',
+          left: 47,
+          top: 23,
+          width: 'max-content',
+          height: 'max-content',
+        }}
+        data-uid='b4a'
+      >
+        woah
+      </span>
+    </div>
   </Storyboard>
 )
